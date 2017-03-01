3b5811de36dcd6a7dfc5035d0fc0daee31654665
QUANVHSE61254\Win <quanvhse61254@fpt.edu.vn>
2017-03-01 11:51:42 +0700

    update code QUan

--- app/src/main/java/com/example/mypc/aaiv_voicecontrol/Constants.java
+++ app/src/main/java/com/example/mypc/aaiv_voicecontrol/Constants.java
@@ -14,6 +14,28 @@ public class Constants {
     public static final String EMOTION_API = "api/emotion/";
     public static final String DATA_API = "data/";
 
+    //Command
+    private static String SHOOTING_COMMAND;
+    private static String DETECT_PERSON_COMMAND;
+    private static String DETECT_OBJECT_COMMAND;
+    private static String DETECT_VIEW_COMMAND;
+    private static String REPEAT_RESULT_COMMAND;
+    private static String NEW_PERSON_COMMAND;
+    private static String ACCEPT_COMMAND;
+    private static String DENY_COMMAND;
+    private static String SHOW_LOG_COMMAND;
+
+//    private static String SHOOTING_COMMAND = "Chụp";
+//    private static String DETECT_PERSON_COMMAND = "Ai đó";
+//    private static String DETECT_OBJECT_COMMAND = "Cái gì đó";
+//    private static String DETECT_VIEW_COMMAND = "Cảnh gì đó";
+//    private static String REPEAT_RESULT_COMMAND = "lặp lại";
+//    private static String NEW_PERSON_COMMAND = "thêm mới";
+//    private static String ACCEPT_COMMAND = "Đồng ý";
+//    private static String DENY_COMMAND = "Hủy";
+//    private static String SHOW_LOG_COMMAND = "lịch sử";
+
+
     public static void setApiHost(String hostApi) {
         API_HOST = "http://" + hostApi + "/CapstoneProject.WebAPI/";
     }
@@ -43,4 +65,78 @@ public class Constants {
                   Logging flag
                  */
     public static final boolean LOGGING = false;
+
+
+    public static void setShootingCommand(String shootingCommand) {
+        SHOOTING_COMMAND = shootingCommand;
+    }
+
+    public static void setDetectPersonCommand(String detectPersonCommand) {
+        DETECT_PERSON_COMMAND = detectPersonCommand;
+    }
+
+    public static void setDetectObjectCommand(String detectObjectCommand) {
+        DETECT_OBJECT_COMMAND = detectObjectCommand;
+    }
+
+    public static void setDetectViewCommand(String detectViewCommand) {
+        DETECT_VIEW_COMMAND = detectViewCommand;
+    }
+
+    public static void setRepeatResultCommand(String repeatResultCommand) {
+        REPEAT_RESULT_COMMAND = repeatResultCommand;
+    }
+
+    public static void setNewPersonCommand(String newPersonCommand) {
+        NEW_PERSON_COMMAND = newPersonCommand;
+    }
+
+    public static void setAcceptCommand(String acceptCommand) {
+        ACCEPT_COMMAND = acceptCommand;
+    }
+
+    public static void setDenyCommand(String denyCommand) {
+        DENY_COMMAND = denyCommand;
+    }
+
+    public static void setShowLogCommand(String showLogCommand) {
+        SHOW_LOG_COMMAND = showLogCommand;
+    }
+
+
+    public static String getShootingCommand() {
+        return SHOOTING_COMMAND;
+    }
+
+    public static String getDetectPersonCommand() {
+        return DETECT_PERSON_COMMAND;
+    }
+
+    public static String getDetectObjectCommand() {
+        return DETECT_OBJECT_COMMAND;
+    }
+
+    public static String getDetectViewCommand() {
+        return DETECT_VIEW_COMMAND;
+    }
+
+    public static String getRepeatResultCommand() {
+        return REPEAT_RESULT_COMMAND;
+    }
+
+    public static String getNewPersonCommand() {
+        return NEW_PERSON_COMMAND;
+    }
+
+    public static String getAcceptCommand() {
+        return ACCEPT_COMMAND;
+    }
+
+    public static String getDenyCommand() {
+        return DENY_COMMAND;
+    }
+
+    public static String getShowLogCommand() {
+        return SHOW_LOG_COMMAND;
+    }
 }
